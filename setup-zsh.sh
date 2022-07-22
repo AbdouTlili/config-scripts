7a18d9d6f3b541838414e2e8c79a0941c12eba77
AbdouTlili <ha_tlili@esi.dz>
2022-07-22 01:38:18 +0000

    zsh checker added with default .zshrc conf file

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,14 +1,44 @@
 
 
+echo Installing Zsh 
 
-which curdl > /dev/null
+echo "1- checking if zsh is installed.."
+echo
+
+which zsh > /dev/null
 
 if [ $? -eq 0 ]
 then 
-    echo "curl installed skipping"
+    echo "zsh installation skipped.."
+else
+    echo "no zsh installed"
+    echo "installing zsh"
+    sudo apt install zsh
+
+    echo "setting default shell to zsh"
+    chsh -s $(which zsh) 
+fi
+
+echo 
+
+echo "2-  checking curl.."
+echo
+
+which curl > /dev/null
+
+if [ $? -eq 0 ]
+then 
+    echo "curl installation skipped.."
 else
     echo "no curl installed"
     echo "installing curl"
     sudo apt install curl 
 fi
 
+echo 
+echo ""
+
+
+
+
+sudo apt install zsh
